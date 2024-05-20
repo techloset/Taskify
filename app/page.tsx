a47ed56bea63945ab2c904a945a84872b732d360
Muhammad Huzaifa Rauf <mhuzaifarauf13@gmail.com>
2024-05-20 10:54:09 +0500

    fix:Fixed some errors

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,50 @@
-import Image from "next/image";
+import { useState } from "react";
+import "./App.css";
+import Header from "./components/header/Header";
+import { BoardContext } from "./contexts/BoardContext";
+import { Sidebar } from "react-feather";
+import { Main } from "next/document";
 
-export default function Home() {
-  return <h1>Hello World</h1>;
+function App() {
+  const boardData = {
+    active: 0,
+    boards: [
+      {
+        name: "My Trello Board",
+        bgcolor: "#069",
+        list: [
+          {
+            id: "1",
+            title: "To do",
+            items: [{ id: "cdrFt", title: "Project Description 1" }],
+          },
+          {
+            id: "2",
+            title: "In Progress",
+            items: [{ id: "cdrFv", title: "Project Description 2" }],
+          },
+          {
+            id: "3",
+            title: "Done",
+            items: [{ id: "cdrFb", title: "Project Description 3" }],
+          },
+        ],
+      },
+    ],
+  };
+  const [allboard, setAllBoard] = useState(boardData);
+
+  return (
+    <>
+      <Header></Header>
+      <BoardContext.Provider value={{ allboard, setAllBoard }}>
+        <div className="content flex">
+          <Sidebar></Sidebar>
+          <Main></Main>
+        </div>
+      </BoardContext.Provider>
+    </>
+  );
 }
+
+export default App;
